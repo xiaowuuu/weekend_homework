3ec6d5bb8b5f0b99e30d58bf3a7a640bcdca7940
ying <huaiy1105@gmail.com>
2023-07-09 20:26:36 +0100

    double check solutions, add numbers to outcomes

--- advanced_logic_exercise.py
+++ advanced_logic_exercise.py
@@ -3,25 +3,29 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # # 1. Print out a list of the even integers:
-# for even_integer in numbers:
-#     if even_integer % 2 == 0:
-
-#         print(even_integer)
+print("Question 1")
+def even_numbers(list):
+    even_list = []
+    for even_integer in numbers:
+        if even_integer % 2 == 0:
+            even_list.append(even_integer)
+    return even_list
+print(even_numbers(numbers))
+            
 
 # # 2. Print the difference between the largest and smallest value:
-# largest_number = max(numbers)
-# smallest_number = min(numbers)
-# difference_value = largest_number - smallest_number
-# print(difference_value)
-
-
+print("Question 2")
+largest_number = max(numbers)
+smallest_number = min(numbers)
+difference_value = largest_number - smallest_number
+print(difference_value)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
-
-# def find_two(list):
-#     for i in range(len(list) - 1):
-#         if list[i] == list[i + 1] and list[i] == 2:
-#             return True
+print("Question 3")
+def find_two(list):
+    for i in range(len(list) - 1):
+        if list[i] == list[i + 1] and list[i] == 2:
+            return True
 
 # solution 2. works on any identical numbers
 # def find_same_number(numbers):
@@ -29,10 +33,7 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 #         if numbers[i] == numbers[i + 1]:
 #             return True
 
-# print(find_two(numbers))
-
-
-
+print(find_two(numbers))
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
@@ -40,20 +41,21 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
 
 # ignore 6 until 7
-# def ignore_six_until_seven(list):
-#     sum = 0
-#     ignore_number = False
-#     for i in range(len(list)):
-#         if list[i] == 6:
-#             ignore_number = True
-#             continue
-#         elif list[i] == 7:
-#             ignore_number = False
-#             continue
-#         if not ignore_number:
-#             sum += list[i]
-#     return sum
-# print(ignore_six_until_seven(numbers))
+print("Question 4")
+def ignore_six_until_seven(list):
+    sum = 0
+    ignore_number = False
+    for i in range(len(list)):
+        if list[i] == 6:
+            ignore_number = True
+            continue
+        elif list[i] == 7:
+            ignore_number = False
+            continue
+        if not ignore_number:
+            sum += list[i]
+    return sum
+print(ignore_six_until_seven(numbers))
 
 # numbers starting with a 6, then how about if there's a 61 in the list?
 
@@ -65,7 +67,7 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 #
 #    So [5, 13, 2] would have sum of 5. 
 
-
+print("Question 5")
 def find_lucky_numbers(list):
     sum_of_number = 0
     unlucky_number = False
